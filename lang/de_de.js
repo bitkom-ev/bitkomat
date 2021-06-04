a7c1fd5750e7e02f859e9c4e2d15173514785f29
Konrad Tadesse <k.tadesse@bitkom.org>
2021-06-04 09:00:23 +0200

    * cleanup: unused function removed; sass style extended, improved;

--- lang/de_de.js
+++ lang/de_de.js
@@ -159,21 +159,3 @@ Existiert sie und enthält keine Syntaxfehler?`;
         return T;
     }();
 
-(function () {
-    let span = document.createElement('span');
-
-    span.className = 'fa';
-    span.style.display = 'none';
-    document.body.insertBefore(span, document.body.firstChild);
-
-    function css(element, property) {
-        return window.getComputedStyle(element, null).getPropertyValue(property);
-    }
-
-    if (css(span, 'font-family') !== 'FontAwesome') {
-        let headHTML = document.head.innerHTML;
-        headHTML += '<link rel="stylesheet" href="third-party/font-awesome/css/font-awesome.min.css">';
-        document.head.innerHTML = headHTML;
-    }
-    document.body.removeChild(span);
-})();
